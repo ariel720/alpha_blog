2ef0d4779d6398b585d9e876124c6c66eca5d719
shinhee kim <ariel720@naver.com>
2016-12-19 01:53:21 +0000

    password authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216024640) do
+ActiveRecord::Schema.define(version: 20161219014353) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20161216024640) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end

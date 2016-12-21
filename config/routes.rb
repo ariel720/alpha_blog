2363a8da5617f73d228b6ce6a17d45028853582f
shinhee kim <ariel720@naver.com>
2016-12-21 02:38:19 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   
   post 'users', to: 'users#create'
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

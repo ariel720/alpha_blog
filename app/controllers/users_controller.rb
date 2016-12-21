cd82b3926a69da33563ba40bd1e7dd897bfa8f8b
shinhee kim <ariel720@naver.com>
2016-12-21 00:34:49 +0000

    update user format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,8 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    debugger
-    @user = User.new(user)
+    
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :email)
+      params.require(:user).permit(:username, :email, :password)
     end
 end

f0f9035bc9a9902c90d0146e8bd9b81a5ee4c7d5
shinhee kim <ariel720@naver.com>
2016-12-21 23:41:14 +0000

    update login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "success"
+            redirect_to user_path(user)
+        else
+            flash[:danger] = "there was something wrong in your login information"
+            render 'new'
+        end
     end
     
     def destroy
+    session[:user_id] = nil
+    redirect_to root_path
     end
-    
+ 
 
 end
